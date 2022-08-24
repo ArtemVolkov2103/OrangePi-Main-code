b749d82074886d3eb7e8093ec41db539645c49cc
ArtemVolkov2103 <mr.folk18@gmail.com>
2022-08-24 08:23:03 +0000

    ok

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -121,7 +121,7 @@ if __name__ == '__main__':
                     # nimers for not a non-stop sending symbols
                     if distance > 30 and distance < 50:
                         s.write(bytes(str(angle) + "," + str(distance), 'utf-8'))
-                        cv2.rectangle(img, (310, 30), (340, 60), (255, 255, 255), 15)
+                        cv2.rectangle(img, (310, 10), (340, 40), (255, 255, 255), 30)
                         cv2.putText(img, "F ", (320, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
 
                 '''if (x > (width / 2 + edge*2)) and x != 0:
