c8206abbbcf6d8cbc088773890161b2d82c3feb9
ArtemVolkov2103 <mr.folk18@gmail.com>
2022-08-24 06:12:03 +0000

    ok

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -115,7 +115,7 @@ if __name__ == '__main__':
                     cv2.circle(img, (x, y), 10, (255, 0, 0), -1)
                     # nimers for not a non-stop sending symbols
                 if h > 230 and (x < (width / 2 + edge * 2)) and (x > (width / 2 - edge * 2)):
-                    s.write(bytes(angle + "," + distance, 'utf-8'))
+                    s.write(bytes(str(angle) + "," + str(distance), 'utf-8'))
                     cv2.putText(img, "F ", (40, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
 
                 '''if (x > (width / 2 + edge*2)) and x != 0:
