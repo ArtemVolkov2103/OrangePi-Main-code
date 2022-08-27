68f93005c4ae33da31cce50fb1672036e909467f
ArtemVolkov2103 <mr.folk18@gmail.com>
2022-08-27 19:17:03 +0000

    ok

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -125,11 +125,11 @@ if __name__ == '__main__':
                         if cameraAng == 0:
                             camEdge = False
                 if dArea > 100 and w > 80 and h > 80:
-                    if angle > cameraAng:
-                        cameraAng = cameraAng - 1
-                        s.write(bytes(str(cameraAng) + "," + str(60), 'utf-8'))
                     if angle < cameraAng:
                         cameraAng = cameraAng + 1
+                        s.write(bytes(str(cameraAng) + "," + str(60), 'utf-8'))
+                    if angle > cameraAng:
+                        cameraAng = cameraAng - 1
                         s.write(bytes(str(cameraAng) + "," + str(60), 'utf-8'))    
                     print("Обьект на " + str(cameraAng) + "," + str(angle))
                     isObjectFound = True
