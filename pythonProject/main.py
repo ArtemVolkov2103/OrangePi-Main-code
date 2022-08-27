6a5e7d3385b69b30edcf882e69843e5df82feabb
ArtemVolkov2103 <mr.folk18@gmail.com>
2022-08-27 17:07:03 +0000

    ok

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -61,9 +61,8 @@ if __name__ == '__main__':
     previousSec = 0.0
 
     while True:
-        if(!isObjectFound){
-        
-        }
+        if(isObjectFound == False)
+
         currentSec = time.perf_counter()
         flag, img = cap.read()
         # width = 640
