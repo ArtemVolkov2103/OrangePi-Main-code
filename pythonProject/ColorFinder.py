481914cd740711df8011deaca606bf598bdb83ed
ArtemVolkov2103 <mr.folk18@gmail.com>
2022-08-28 14:10:03 +0000

    ok

--- pythonProject/ColorFinder.py
+++ pythonProject/ColorFinder.py
@@ -7,14 +7,14 @@ while(1):
     # Convert BGR to HSV
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     # define range of blue color in HSV
-    lower_blue = np.array([110,50,50])
-    upper_blue = np.array([130,255,255])
+    lower_blue = np.array([203, 57, 81])
+    upper_blue = np.array([190, 83, 41])
     # Threshold the HSV image to get only blue colors
     mask = cv.inRange(hsv, lower_blue, upper_blue)
     # Bitwise-AND mask and original image
     res = cv.bitwise_and(frame,frame, mask= mask)
-    cv.imshow('frame',frame)
-    cv.imshow('mask',mask)
+    #cv.imshow('frame',frame)
+    #cv.imshow('mask',mask)
     cv.imshow('res',res)
     k = cv.waitKey(5) & 0xFF
     if k == 27:
