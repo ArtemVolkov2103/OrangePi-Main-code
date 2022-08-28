399239fe10b8e24ac9d397f27ec60c05fc67eba7
ArtemVolkov2103 <mr.folk18@gmail.com>
2022-08-28 17:50:03 +0000

    ok

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -126,18 +126,18 @@ if __name__ == '__main__':
                     if angle > 8 and cameraAng < 180:
                         cameraAng = cameraAng + 1
                         s.write(bytes(str(cameraAng) + "," + str(60), 'utf-8'))
-                        #sleep(0.1)
+                        sleep(0.05)
                         print("Обьект на " + str(cameraAng))
 
                     if angle < -8  and cameraAng > 0:
                         cameraAng = cameraAng - 1
                         s.write(bytes(str(cameraAng) + "," + str(60), 'utf-8'))
-                        #sleep(0.1)
+                        sleep(0.05)
                         print("Обьект на " + str(cameraAng))
 
                     #print("Обьект на " + str(cameraAng))
                     if not isRotated:
-                        #sleep(0.1)
+                        sleep(0.05)
                         if cameraAng > 90:
                         	print("Left " + str(cameraAng))
                         	s.write(bytes(str(cameraAng) + "," + str(70), 'utf-8'))
