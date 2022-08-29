104791e29393f4e595250dbcf35412e8d545d822
ArtemVolkov2103 <mr.folk18@gmail.com>
2022-08-29 08:24:04 +0000

    ok

--- pythonProject/test.py
+++ pythonProject/test.py
@@ -22,7 +22,11 @@ s.open()
 sleep(1)
 s.write(bytes(str(180) + "," + str(70), 'utf-8'))
 while(1):
-	for i in range(90,180):
+	for i in range(90, 180):
+		s.write(bytes(str(i) + "," + str(60), 'utf-8'))
+		print(i)
+		sleep(0.1)
+	for i in range(180, 90):
 		s.write(bytes(str(i) + "," + str(60), 'utf-8'))
 		print(i)
 		sleep(0.1)
