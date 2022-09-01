de7575e3f6d571131a6b5ef959438e7d03cd18fe
ArtemVolkov2103 <mr.folk18@gmail.com>
2022-09-01 14:59:03 +0000

    ok

--- pythonProject/FormDeterminant.py
+++ pythonProject/FormDeterminant.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
         #resized = imutils.resize(img_copy, width=300)
         #ratio = cap.shape[0] / float(resized.shape[0])
 
-        hsv = cv2.cvtColor(img_copy, cv2.COLOR_BGR2HSV)
+        hsv = cv2.cvtColor(np.float32(img_copy), cv2.COLOR_BGR2HSV)
 
         thres = cv2.inRange(hsv, low_blue, high_blue)
         thres = cv2.GaussianBlur(thres, (5, 5), 0)
