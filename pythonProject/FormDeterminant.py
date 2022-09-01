cc7a4e2c974a6a841669fc98c96b2e880b984e61
ArtemVolkov2103 <mr.folk18@gmail.com>
2022-09-01 09:36:03 +0000

    ok

--- pythonProject/FormDeterminant.py
+++ pythonProject/FormDeterminant.py
@@ -29,57 +29,55 @@ if __name__ == '__main__':
     def nothing(*arg):
         pass
 
-	cv2.namedWindow("out_window")
-		cap = video.create_capture(1)
-		#cap = video.create_capture(0) #для ноутбука
+	cv2.namedWindow("out_window") cap = video.create_capture(1) 
 
-		cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-		cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+	cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+	cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 		
+	height, width = img.shape[:2]
+    high_blue = numpy.array((104, 255, 255), numpy.uint8)
+    low_blue = numpy.array((82, 140, 88), numpy.uint8)
+    while(True):
+		flag, img = cap.read()
 		height, width = img.shape[:2]
-        edge = 100
+		hsv = cv2.cvtColor(resized, cv2.COLOR_BGR2HSV)
 
-        #low_blue = numpy.array((90, 70, 70), numpy.uint8)
-        #high_blue = numpy.array((140, 255, 255), numpy.uint8)
-        high_blue = numpy.array((104, 255, 255), numpy.uint8)
-        low_blue = numpy.array((82, 140, 88), numpy.uint8)
-		
-		while(True):
-			flag, img = cap.read()
-			# width = 640
-			# height = 480
-
-			height, width = img.shape[:2]
-			hsv = cv2.cvtColor(resized, cv2.COLOR_BGR2HSV)
-
-			thres = cv2.inRange(hsv, low_blue, high_blue)
-			thres = cv2.GaussianBlur(thres, (5, 5), 0)
-			cnts = cv2.findContours( 
+		thres = cv2.inRange(hsv, low_blue, high_blue)
+		thres = cv2.GaussianBlur(thres, (5, 5), 0)
+		cnts = cv2.findContours( 
                                  thres.copy(), 
                                  cv2.RETR_EXTERNAL, 
                                  cv2.CHAIN_APPROX_SIMPLE 
                                 )                       
-			cnts = imutils.grab_contours(cnts)
-			for c in cnts:
-				if cv2.contourArea(c) < BLOBSIZE:
-					continue
-				c = c.astype("float")
-				c *= ratio
-				c = c.astype("int")
+		cnts = imutils.grab_contours(cnts)
+		for c in cnts:
+			if cv2.contourArea(c) < BLOBSIZE:
+				continue
+			c = c.astype("float")
+			c *= ratio
+			c = c.astype("int")
 
-				cv2.drawContours(img_copy, [c], -1, CONTCOLOR, CTHICK)
+			cv2.drawContours(img_copy, [c], -1, CONTCOLOR, CTHICK)
 
-				if cv2.contourArea(c) < BLOBSIZE:
-					continue
+			if cv2.contourArea(c) < BLOBSIZE:
+				continue
 
-				M = cv2.moments(c)
-				cX = 0
-				cY = 0
+			M = cv2.moments(c)
+			cX = 0
+			cY = 0
 
-				if M["m00"] != 0:
-					cX = int((M["m10"] / M["m00"]) * ratio)
-					cY = int((M["m01"] / M["m00"]) * ratio)
+			if M["m00"] != 0:
+				cX = int((M["m10"] / M["m00"]) * ratio)
+				cY = int((M["m01"] / M["m00"]) * ratio)
 
-				shapename = shapeDetect(c)
-				shapename = hue + " " + shapename
+			shapename = shapeDetect(c)
+			shapename = hue + " " + shapename
+			cv2.imshow("Image", img_copy)
+
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
+vs.stop()
         
