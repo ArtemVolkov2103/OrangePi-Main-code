2a55c75558271f1926523b3b2faf4080b56c507c
ArtemVolkov2103 <mr.folk18@gmail.com>
2022-09-01 14:52:03 +0000

    ok

--- pythonProject/FormDeterminant.py
+++ pythonProject/FormDeterminant.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
         flag, img = cap.read()
         height, width = img.shape[:2]
         
-        img_copy = cap.copy()
+        img_copy = cap
         resized = imutils.resize(image, width=300)
         ratio = image.shape[0] / float(resized.shape[0])
 
