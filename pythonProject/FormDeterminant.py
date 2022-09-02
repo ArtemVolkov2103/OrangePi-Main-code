9cf59f80e3677277f58c082426eace5f40d9b8f0
ArtemVolkov2103 <mr.folk18@gmail.com>
2022-09-02 18:10:03 +0000

    ok

--- pythonProject/FormDeterminant.py
+++ pythonProject/FormDeterminant.py
@@ -61,10 +61,11 @@ if __name__ == '__main__':
         ratio = 1
         
         hsv = cv2.cvtColor(img_copy, cv2.COLOR_BGR2HSV)
+        hsv = cv2.medianBlur(hsv, 7)
 
         thres = cv2.inRange(hsv, low_blue, high_blue)
         #thres = cv2.GaussianBlur(thres, (5, 5), 0)
-        thres = cv2.medianBlur(thres, 7)
+        #thres = cv2.medianBlur(thres, 7)
         
         #edged = cv2.Canny(thres, 10, 250)
         
