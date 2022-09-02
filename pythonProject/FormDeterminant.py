332b5b782818bda39b71fbc44546cbe1667a2e4b
ArtemVolkov2103 <mr.folk18@gmail.com>
2022-09-02 17:42:03 +0000

    ok

--- pythonProject/FormDeterminant.py
+++ pythonProject/FormDeterminant.py
@@ -62,14 +62,14 @@ if __name__ == '__main__':
         
         hsv = cv2.cvtColor(img_copy, cv2.COLOR_BGR2HSV)
         gray = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
-        gray = cv2.medianBlur(gray, 7)
+        gray = cv2.medianBlur(gray, 11)
         thres = cv2.inRange(hsv, low_blue, high_blue)
         #thres = cv2.GaussianBlur(thres, (5, 5), 0)
         thres = cv2.medianBlur(thres, 7)
         
         edged = cv2.Canny(gray, 200, 250)
         
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 30))
         closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
         cnts = cv2.findContours( 
                                  closed.copy(), 
