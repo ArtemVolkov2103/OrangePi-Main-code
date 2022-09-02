bcc7ba8f4983c9821379b95d217d0eeb191ee1aa
ArtemVolkov2103 <mr.folk18@gmail.com>
2022-09-02 09:55:03 +0000

    ok

--- pythonProject/FormDeterminant.py
+++ pythonProject/FormDeterminant.py
@@ -73,7 +73,7 @@ if __name__ == '__main__':
         #result2 = cv2.inpaint(img, mask2, 0.1, cv2.INPAINT_TELEA)
         
         hsv = cv2.cvtColor(img_copy, cv2.COLOR_BGR2HSV)
-		edged = cv2.Canny(hsv, 10, 250)
+        edged = cv2.Canny(hsv, 10, 250)
         thres = cv2.inRange(hsv, low_blue, high_blue)
         #thres = cv2.GaussianBlur(thres, (5, 5), 0)
         thres = cv2.medianBlur(thres, 7)
