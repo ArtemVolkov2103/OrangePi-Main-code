bb8de74c05dff40c95ed22fe04c1df1d7859a69d
ArtemVolkov2103 <mr.folk18@gmail.com>
2022-09-02 18:12:03 +0000

    ok

--- pythonProject/FormDeterminant.py
+++ pythonProject/FormDeterminant.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
 
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-    high_blue = numpy.array((194, 255, 255), numpy.uint8)
+    high_blue = numpy.array((124, 255, 255), numpy.uint8)
     low_blue = numpy.array((70, 125, 75), numpy.uint8)
     while(True):
         flag, img = cap.read()
@@ -64,7 +64,7 @@ if __name__ == '__main__':
 
         thres = cv2.inRange(hsv, low_blue, high_blue)
         #thres = cv2.GaussianBlur(thres, (5, 5), 0)
-        thres = cv2.medianBlur(thres, 15)
+        thres = cv2.medianBlur(thres, 13)
         
         #edged = cv2.Canny(thres, 10, 250)
         
