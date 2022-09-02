ba9e2b437c8b305cab2cf7ce6ab035e588965295
ArtemVolkov2103 <mr.folk18@gmail.com>
2022-09-02 17:40:04 +0000

    ok

--- pythonProject/FormDeterminant.py
+++ pythonProject/FormDeterminant.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
         #thres = cv2.GaussianBlur(thres, (5, 5), 0)
         thres = cv2.medianBlur(thres, 7)
         
-        edged = cv2.Canny(gray, 10, 250)
+        edged = cv2.Canny(gray, 100, 250)
         
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
         closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
